b05e687fa30145ccb9b35c6227a55f1258f8e3e6
fawad07 <muhammad.f.aleem@gmail.com>
2024-06-26 14:43:18 -0700

    config/ vite -v 5

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+//Main.jsx
+import './Main.css';
+
+
+function Main() {
+    return (
+        <div className="container">
+            <p>This is your first Vite project </p>
+            <p>"Vite" means "quick" in French</p>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/utils/constants.js
@@ -0,0 +1,14 @@
+export const data = [
+	{
+		name: "shark",
+		image: new URL("../assets/animals/shark.png", import.meta.url).href,
+	},
+	{
+		name: "wolf",
+		image: new URL("../assets/animals/wolf.png", import.meta.url).href,
+	},
+	{
+		name: "default",
+		image: new URL("../assets/animals/crab.png", import.meta.url).href,
+	},
+];
