397a86ba44f7af078fee478b828dcee3be20d63c
fawad07 <muhammad.f.aleem@gmail.com>
2024-07-02 09:55:44 -0700

    add/Main styles, ItemCard Styles

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,10 +1,12 @@
+import "./ItemCard.css";
+
 
 function ItemCard( { props }) {
    console.log(props.name);            //debugging statement
   return ( 
-        <div>
-            <h2> {props.name}</h2>
-            <img src={props.link} alt={props.name} />  
+        <div className="cards__container">
+            <h2 className="cards__title"> {props.name}</h2>
+            <img src={props.link} alt={props.name} className="cards__image" />  
         </div>
     );
 }
